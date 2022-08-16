f43eb52ec1f1815cd610183c9c8a5298a62e562b
Lenx0 <thiagofreitas1106@gmail.com>
2022-08-16 15:34:22 -0300

    upload

--- src/app/components/pages/dashboard/Dashboard.jsx
+++ src/app/components/pages/dashboard/Dashboard.jsx
@@ -1,182 +1,58 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
-import moment from "moment";
-import "./Dashboard.css";
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
+import { React, useState, useEffect } from "react";
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-
-// const headerProps = {
-//   icon: "users",
-//   title: "Vagas disponíveis",
-//   subtitle: "Painel de aprovação",
-// };
-
-//const baseUrl = "http://localhost:3005/v1/agesp";
-const baseUrl = "https://aggesp-api.altogiro.net/v1/agesp"
-const initialState = {
-  requester: [{
-    vacancyDateOpen: "",
-    positionOrFunction: "",
-    sector: "",
-    manager: "",
-    responsible: "",
-    hiringReason: "",
-    replacedEmployee: "",
-    initialSalary: "",
-    status: "",
-    admissionDate: "",
-    vacancyDateClose: "",
-  }],
-  list: [],
-};
-
-export default class Vacancies extends Component {
-  state = { ...initialState };
-
-  componentWillMount() {
-    axios(`${baseUrl}/vacancy${"/list"}`).then((resp) => {
-      this.setState({ list: resp.data });
-      console.log(`Dados da lista:${JSON.stringify(resp.data)}`);
+useEffect(() => {
+  fetch("https://aggesp-api.altogiro.net/v1/agesp/vacancy/list")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(resp);
+      setData(resp);
     });
-  }
+}, []);
 
-  clear() {
-    this.setState({ requester: initialState.requester });
-  }
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
-
-  save() {
-    const requester = this.state.requester;
-    const method = requester._id ? "put" : "post";
-    const url = requester._id
-      ? `${baseUrl}/user/update/${requester._id}`
-      : `${baseUrl}/user/create/`;
-    console.log(method);
-    console.log(url);
-    console.log(requester);
-    axios[method](url, requester).then((resp) => {
-      const list = this.getUpdatedList(resp.data);
-      this.setState({ requester: initialState.requester, list });
-      console.log(resp.data);
-      console.log(list);
-    });
-  }
-
-  getUpdatedList(requester, add = true) {
-    const list = this.state.list.filter((u) => u._id !== requester._id);
-    if (add) list.unshift(requester);
-    return list;
-  }
-
-  updateField(event) {
-    const requester = { ...this.state.requester };
-    requester[event.target.name] = event.target.value;
-    this.setState({ requester });
-  }
-
-  renderForm() {
-    return (
-      <div className="title-container">
-        <h1>Lista de vagas cadastradas</h1>
-      </div>
-    );
-  }
-
-  load(requester) {
-    this.setState({ requester });
-  }
-
-  remove(requester) {
-    axios.delete(`${baseUrl}/vacancy/delete/${requester._id}`).then((resp) => {
-      const list = this.getUpdatedList(requester, false);
-      this.setState({ list });
-    });
-  }
-
-  renderTable() {
-    return (
-      <div className="scrollbar scrollbar-lady-lips">
-        <div className="force-overflow">
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th>Ações</th>
-            <th>Abertura</th>
-            <th>Cargo</th>
-            <th>Setor</th>
-            <th>Gestor</th>
-            <th>Responsável</th>
-            <th>Motivo</th>
-            <th>A ser substituído</th>
-            <th>Salário</th>
-            <th>Status</th>
-            <th>Admitido</th>
-            <th>Fechamento</th>
-          </tr>
-        </thead>
-        <tbody>{this.renderRows()}</tbody>
-      </table>
-      </div>
-      </div>
-    );
-  }
-
-  renderRows() {
-    return this.state.list.map((requester) => {
-      return (
-        <tr key={requester._id}>
-          <td>
-            {/* <button
-              className="btn btn-success ml-2"
-              id="btn-check"
-              onClick={() => this.load(requester)}
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <i className="fa-solid fa-circle-check"></i>
-            </button>
-            <button
-              className="btn btn-danger ml-2"
-              id="btn-delete"
-              onClick={() => this.remove(requester)}
-            >
-              <i className="fa-solid fa-circle-xmark"></i>
-            </button> */}
-            <button
-              className="btn btn-warning ml-2"
-            >
-              <i className="fa-solid fa-print"></i>
-            </button>
-          </td>
-          <td>{moment(requester.vacancyDateOpen).format('DD.MM.YYYY')}</td>
-          <td>{requester.positionOrFunction}</td>
-          <td>{requester.sector}</td>
-          <td>{requester.manager}</td>
-          <td>{requester.responsible}</td>
-          <td>{requester.hiringReason}</td>
-          <td>{requester.replacedEmployee}</td>
-          <td>{requester.initialSalary}</td>
-          <td>{requester.status}</td>
-          <td>{moment(requester.admissionDate).format('DD.MM.YYYY')}</td>
-          <td>{moment(requester.vacancyDateClose).format('DD.MM.YYYY')}</td>
-        </tr>
-      );
-    });
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        {this.renderForm()}
-        {this.renderTable()}
-      </BrowserRouter>
-    );
-  }
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
